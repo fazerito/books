735bf4e41c0267d59050515067862db495dfc140
Marcin <gervvaz@gmail.com>
2018-07-18 22:09:39 +0200

    Added login manager

--- books/__init__.py
+++ books/__init__.py
@@ -1,6 +1,8 @@
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 import os
 
@@ -11,6 +13,9 @@ app.config["SECRET_KEY"] = "c805bb1ada0664c4ec07143114b523ce"
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
-#db.init_app(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "login"
+login_manager.login_message_category = "info"
 
 from books import routes
