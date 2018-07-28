b7925bca9049ee1593ef394378e8d62715c6a69c
Marcin <gervvaz@gmail.com>
2018-07-28 16:01:33 +0200

    Added book and API routes

--- books/routes.py
+++ books/routes.py
@@ -1,12 +1,15 @@
-from flask import render_template, url_for, flash, redirect, request, session
+from flask import render_template, url_for, flash, redirect, request, session, jsonify
 from books import app, db, bcrypt
-from books.forms import RegistrationForm, LoginForm
+from books.forms import RegistrationForm, LoginForm, SearchForm
 from flask_session import Session
 from sqlalchemy.orm import scoped_session, sessionmaker
 from books.models import *
 from flask_login import login_user, current_user, logout_user, login_required
+import requests
+import json
 
 Session(app)
+
 
 @app.route("/")
 def index():
@@ -49,11 +52,41 @@ def logout():
 @app.route("/search", methods=["POST", "GET"])
 @login_required
 def search():
+    form = SearchForm()
+    books = []
     if request.method == "POST":
-        title = request.form.get("title")
-        book = Book.query.filter_by(title=title).first()
-        author = book.author
-        year = book.year
-        return render_template("book.html", title=title, author=author, year=year)
-    else:
-        return render_template("search.html")
+        choice = form.select.data
+        search_string = form.search.data
+        if choice == "ISBN":
+            books = Book.query.filter(Book.isbn.like("%" + search_string + "%")).all()
+        if choice == "Title":
+            books = Book.query.filter(Book.title.like("%" + search_string + "%")).all()
+        if choice == "Author":
+            books = Book.query.filter(Book.author.like("%" + search_string + "%")).all()
+        return render_template("results.html", books=books)
+    return render_template("search.html", method=["POST"], form=form)
+
+@app.route("/results")
+def results(books):
+    return render_template("results.html", books=books)
+
+@app.route("/book/<string:title>")
+@login_required
+def book(title):
+    book = Book.query.filter_by(title=title).first()
+    title = book.title
+    return render_template("book.html", title=title, book=book)
+
+@app.route("/api/<string:isbn>")
+def api(isbn):
+    book = Book.query.filter_by(isbn=isbn).first()
+    res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "vM2JXxPvhe3OfoiR9dvPg", "isbns": "1632168146"})
+    data = res.json()
+    return jsonify(
+    title=book.title,
+    author=book.author,
+    year=book.year,
+    isbn=book.isbn,
+    review_count=data["books"][0]["work_ratings_count"],
+    average_score=data["books"][0]["average_rating"]
+    )
