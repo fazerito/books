94749a9e449eded394db3831c6937ec0b966e0e7
Marcin <gervvaz@gmail.com>
2018-08-03 12:35:19 +0200

    Added review class

--- books/models.py
+++ books/models.py
@@ -25,6 +25,14 @@ class User(db.Model, UserMixin):
     name = db.Column(db.String, nullable=False)
     password = db.Column(db.String, nullable=False)
     email = db.Column(db.String, nullable=False)
+    reviews = db.relationship("Review", backref="author", lazy=True)
 
     def __repr__(self):
         return f"User({self.name}, {self.email})"
+
+class Review(db.Model):
+    __tablename__ = "reviews"
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String, nullable=False)
+    rating = db.Column(db.String, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
